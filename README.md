edd58ab7b20e368788df8a9f824a8e3a35d0f550
andrew cohen <cohen.andrew.n@gmail.com>
2023-02-21 00:30:22 -0500

    fix: temp

--- README.md
+++
@@ -1,2 +0,0 @@
-# platform
-This is the mono repo for the native and web versions of the platform
--- db/scripts/down.bash
+++
@@ -1,9 +0,0 @@
-while getopts e: flag
-do
-    case "${flag}" in
-        e) env=${OPTARG};;
-    esac
-done
-
-source "$env"
-migrate -path ../migrations -database $DATABASE_URL down -all
