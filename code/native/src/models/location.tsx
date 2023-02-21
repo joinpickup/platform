2b6a830f0085702ef4ffdacb504268074e168d19
andrew cohen <cohen.andrew.n@gmail.com>
2023-02-21 00:22:20 -0500

    fix: clear cache | add gitignore

--- code/native/src/models/location.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "./image";
-import Space from "./space";
-import Person from "./person";
-import { Object } from "./object";
-
-export interface Location {
-  id: number;
-  common_name: string;
-}
--- code/native/src/models/space.tsx
+++
@@ -1,4 +0,0 @@
-export default interface Space {
-  id: number;
-  name: string;
-}
